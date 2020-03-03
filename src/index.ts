484a303d5df4bcd6067ece6da34796e48209bc5c
laihaibo <2466006555@qq.com>
2020-03-03 17:24:36 +0800

    url参数处理

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { bulidURL } from './helpers/url'
+
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return bulidURL(url, params)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+}
 
 export default function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
 }
