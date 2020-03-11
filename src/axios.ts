b960ea14e112a0edc80455f9cb348fd2f13c6843
laihaibo <2466006555@qq.com>
2020-03-11 11:51:02 +0800

    update 1. 扩展接口 2. 函数重载 3. 响应数据支持泛型

--- src/axios.ts
+++ src/axios.ts
@@ -1,38 +1,16 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import xhr from './xhr'
-import { bulidURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
+import { AxiosInstance } from './types'
+import Axios from './core/Axios'
+import { extend } from './helpers/util'
 
-// 处理config
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
-  config.data = transformRequestData(config)
-  config.headers = transformHeaders(config)
+function createInstance(): AxiosInstance {
+  const context = new Axios()
+  const instance = Axios.prototype.request.bind(context)
+
+  extend(instance, context)
+
+  return instance as AxiosInstance
 }
-// 处理url
-function transformUrl(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return bulidURL(url, params)
-}
-// 处理数据
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-// 处理headers
-function transformHeaders(config: AxiosRequestConfig) {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-// 处理data
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
-}
-// 导出axios
-export default function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
+
+const axios = createInstance()
+
+export default axios
